adc538c9b399c54ca8e4096e5b829f67ae854b7e
NicholasKung <Nickung1212@gmail.com>
2020-03-31 13:30:04 -0400

    edited pluralization

--- db/migrate/20200330180303_add_info_table.rb
+++ db/migrate/20200330180303_add_info_table.rb
@@ -1,10 +1,10 @@
 class AddInfoTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :applicant do |t|
+    create_table :applicants do |t|
     t.string :first, null: false
     t.string :last, null: false
     t.text :description, null: false
-    
+
     t.timestamps null: false
     end
   end
