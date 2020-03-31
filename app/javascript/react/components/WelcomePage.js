f89135ea6ead0d69f761825c13172202869df877
NicholasKung <Nickung1212@gmail.com>
2020-03-31 13:28:43 -0400

    Simple welcome page

---
+++ app/javascript/react/components/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WelcomePage = (props) => {
+
+  return(
+    <div>
+      <h1 className = "welcome-title">Click Below to Start the quick application</h1>
+    </div>
+  )
+}
+
+export default WelcomePage
