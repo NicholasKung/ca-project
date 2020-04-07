0c8f79fa73a52330b93cb3053c7cc65f69df23c1
NicholasKung <Nickung1212@gmail.com>
2020-04-07 10:35:04 -0400

    New applicant container

---
+++ app/javascript/react/components/NewApplicantContainer.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
+import NewApplicantForm from './NewApplicantForm.js'
+
+const NewApplicantContainer = (props) => {
+  const [ applicants, setApplicants ] = useState([])
+
+
+  const submitNewApplicant = (formPayLoad) => {
+    debugger
+    fetch('/api/v1/applicants.json', {
+      credentials: "same-origin",
+      method: 'POST',
+      body: JSON.stringify(formPayLoad),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+        error = new Error(errorMessage)
+        throw error
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      debugger
+      setApplicants([
+        ...applicants,
+        body])
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+
+  return (
+    <div>
+      <h3>Add a new card</h3>
+      <NewApplicantForm
+        onSubmit = {submitNewApplicant}
+      />
+    </div>
+  )
+}
+
+export default NewApplicantContainer
