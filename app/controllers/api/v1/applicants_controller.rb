bfd524e96651ed96d1538a06b55dc9ccf2bf1c9f
NicholasKung <Nickung1212@gmail.com>
2020-04-07 10:34:30 -0400

    Create a create method

--- app/controllers/api/v1/applicants_controller.rb
+++ app/controllers/api/v1/applicants_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::ApplicantsController < ApplicationController
   def index
     render json: Applicant.all
   end
+
+  def create
+    applicant = Applicant.new(applicant_params)
+    if applicant.save
+      render json: { applicant: applicant }
+    else
+      render json: { error: applicant.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def applicant_params
+    params.permit(:first, :last, :description)
+  end
 end
