ad52a6ff64c94908b4df70b748139b3a45685a78
NicholasKung <Nickung1212@gmail.com>
2020-04-13 13:50:05 -0400

    Added delete route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
 namespace :api do
     namespace :v1 do
-      resources :applicants, only: [:index, :create] do
+      resources :applicants, only: [:index, :create, :destroy] do
       end
     end
   end
